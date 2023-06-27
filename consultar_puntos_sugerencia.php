3ec0cafa63eadaa0bf7c145952acae57460c9e41
gvillanuevap <gvillanuevap@enerya.com>
2023-06-27 10:34:13 -0600

    Realice un consulta en la tabla no factible, esto con el objetivo de poder mostrar los puntos de no factibles en las sugerencias correcpondientes del colaborador

--- consultar_puntos_sugerencia.php
+++ consultar_puntos_sugerencia.php
@@ -7,6 +7,7 @@
     $producto = 0;
     $puntos_cuanti=0;
     $puntos_cuali=0;
+    $no_factible=0;
     
             $consulta = "SELECT * FROM impacto_cuantitativo_sugerencias WHERE id_concentrado = '$id_concentrado'";
             $query = mysqli_query($conexion,$consulta);
@@ -24,12 +25,20 @@
                 }
             }
 
+            $consulta = "SELECT * FROM no_factible_puntos WHERE id_concentrado = '$id_concentrado'";
+            $query = mysqli_query($conexion,$consulta);
+            if(mysqli_num_rows($query)>0){
+                while ($fila=$query -> fetch_array()){
+                    $no_factible = $fila['puntos'];
+                }
+            }
+
         if($puntos_cuanti!=0){
             $producto = $puntos_cuanti;
-        }else{
-            if($puntos_cuali!=0){
+        }else if($puntos_cuali!=0){
                 $producto = $puntos_cuali;
-            }
+        }else if($no_factible!=0){
+            $producto = $no_factible;
         }
         
 
