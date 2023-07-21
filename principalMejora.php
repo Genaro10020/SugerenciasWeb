2de4ff840034a5c6db0b2024272ba0d79c89ad5a
gvillanuevap <gvillanuevap@enerya.com>
2023-07-21 11:49:51 -0600

    cree el boton para el filtro

--- principalMejora.php
+++ principalMejora.php
@@ -849,7 +849,14 @@ if ($_SESSION["usuario"] && $_SESSION["tipo"]=="Admin"){
                                             <thead class="encabezado-tabla text-center text-light ">
                                                 <tr >
                                                     <th scope="col" class="sticky">Pendiente</th>
-                                                    <th scope="col">Folio<span class="badge bg-primary">*</span></th>
+                                                    <th scope="col">Folio
+                                                        <button class="btn-filtrar border rounded">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-short" viewBox="0 0 16 16">
+                                                                <path fill-rule="evenodd" d="M8 12a.5.5 0 0 0 .5-.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 .5.5z"/>
+                                                            </svg>
+                                                            <label class="label-asc">ASC</label>
+                                                        </button>
+                                                    </th>
                                                     <th scope="col">Planta <span  class="badge bg-primary">*</span></th>
                                                     <th scope="col">Área<span class="badge bg-primary">*</span></th>
                                                     <th scope="col">Subárea<span class="badge bg-primary">*</span></th>
