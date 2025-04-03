17ecc48ffd725367e9bbf67236b7569b2bb4729d
Genaro Villanueva Perez <gvillanuevap@enerya.com>
2025-04-03 11:03:20 -0600

    Se reseteó la variable 'fecha_factibilidad' al dar click en cambiar  a 'En factibilidad'

--- guardar_actualizar_cambiar_a_En_Factibilidad.php
+++ guardar_actualizar_cambiar_a_En_Factibilidad.php
@@ -6,7 +6,7 @@ $id_concentrado=$variables['id_concentrado'];
 $hoy= date('Y-m-d');
 $fecha_limite= date("Y-m-d",strtotime($hoy."+ 8 days")); //agregando 7 dias
 include "conexionGhoner.php";
-        $actualizar= "UPDATE concentrado_sugerencias SET cumplimiento=0, status = 'En Factibilidad', causa_no_factibilidad = '', respuesta_analista = '', check_mc='', fecha_de_inicio='$hoy', fecha_limite='$fecha_limite' WHERE id='$id_concentrado'";
+        $actualizar= "UPDATE concentrado_sugerencias SET cumplimiento=0, status = 'En Factibilidad', causa_no_factibilidad = '', respuesta_analista = '', fecha_factibilidad = '', check_mc='', fecha_de_inicio='$hoy', fecha_limite='$fecha_limite' WHERE id='$id_concentrado'";
         $query = mysqli_query( $conexion, $actualizar);
         $resultado = $query;
         if($resultado){
