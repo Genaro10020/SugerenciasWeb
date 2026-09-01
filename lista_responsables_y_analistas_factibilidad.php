e7c1180a0c5bd71728e90e72ee5bbec8c46c0223
JOSE ANGEL LUGO ESPARZA <jlugoe@enerya.com>
2026-09-01 14:22:40 -0600

    feat(php): se modifico la consulta para ocultar el usuario dependiendo si esta activo o no

--- lista_responsables_y_analistas_factibilidad.php
+++ lista_responsables_y_analistas_factibilidad.php
@@ -4,7 +4,7 @@ header("Content-Type: application/json");
 $variables = json_decode(file_get_contents('php://input'), true);
 include "conexionGhoner.php";
 $analistas=[];
-$consulta = "SELECT * FROM usuarios_sugerencias WHERE tipo = 'Analista' || tipo = 'Responsable'" ;
+$consulta = "SELECT * FROM usuarios_sugerencias WHERE (tipo = 'Analista' OR tipo = 'Responsable') AND status = 'activo'";
 $resultado = mysqli_query($conexion,$consulta);
 while($datos=mysqli_fetch_array($resultado)){
     $analistas[] = $datos;
